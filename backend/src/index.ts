9204716346867df7431a5705e7d141227017e33d
Alyssandria <sanchez.roniel.work@gmail.gmail>
2026-09-09 17:54:57 +0800

    chore*: add zod schema and tsconfig changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,14 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
-import resultsRoutes from "./routes/results.routes.js";
+import appRouter from "./routes/index.routes.js";
 const PORT = Number(process.env.PORT) || 3001;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/results", resultsRoutes);
+app.use('/api', appRouter);
 
 app.listen(PORT, () => {
   console.log(`backend listening on http://localhost:${PORT}`);
